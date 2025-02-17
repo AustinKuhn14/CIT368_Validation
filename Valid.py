65cf7836226160584fc86f4a55ef8a3832c675ff
AustinKuhn14 <atk14@pct.edu>
2025-02-17 18:00:01 -0500

    Add files via upload

---
+++ Valid.py
@@ -0,0 +1,55 @@
+import re
+
+import re._compiler
+
+class Valid:
+  """
+  Validate a ZIP code. return true if the input is a valid ZIP, 
+  false otherwise.
+
+  input: string
+  output: boolean
+
+  TODO: implement a method to validate a ZIP code
+  """
+  @staticmethod
+  def zip(input):
+    #length?
+    if len(input) != 5:
+      print("Zip length is invalid!")
+      return False
+    #digits?
+    if not input.isdigit():
+      print("Enter numbers for Zip")
+      return False
+    #real ZIP?
+    
+    return True
+  
+  """
+  Validate a ZIP code. return true if the input is a valid ZIP, 
+  false otherwise.
+
+  input: string
+  output: boolean
+
+  TODO: implement a method to validate a ZIP code
+  """
+  @staticmethod
+  def phone(input):
+    #length?
+    if len(input) != 10:
+      print("invalid Phone# length")
+      return False
+    #digits?
+    if not input.isdigit():
+      print("Enter numbers for Phone#")
+      return False
+    #format?
+    reg = r'/^[0-9]{10}$/'
+    if not re.match(reg,input):
+      print("Invalid phone number try something like 5556667777")
+      return False
+    #real number?
+    
+    return True
